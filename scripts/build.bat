18f2c2c2a6fab80b866709c5ce5655c2232d9a58
Christophe VILA <christophe.vila@thalesgroup.com>
2020-03-23 17:21:58 +0100

    updates

--- scripts/build.bat
+++ scripts/build.bat
@@ -2,6 +2,6 @@
 REM pkg/fyne/icon/jira.go is generated with fyne bundle jira.png > jira.go
 REM jirop.syso is generated with rcsc -icon jira.ico
 set CC=C:\Programs\Cygwin\bin\x86_64-w64-mingw32-gcc.exe
-pushd cmd\jirop
-go build -ldflags -H=windowsgui -o ..\..\jirop.exe
+pushd ..\cmd\jirop
+go install -ldflags -H=windowsgui
 popd
